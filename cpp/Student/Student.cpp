2e2d05a3db9916edcded236f85086117161a357d
Oceannnx <onopingping@gmail.com>
2023-01-24 10:59:10 +0700

    final 1

--- cpp/Student/Student.cpp
+++ cpp/Student/Student.cpp
@@ -1,28 +1,46 @@
 #include<iostream>
 #include <bits/stdc++.h>
+#include <cmath>
 using namespace std;
 
 struct Student{
     string name;
     int score;
+    char grade;
 };
 
 double MaxStudent(Student student[],int size){
     int max_score = student[0].score;
     for(int i=0; i<size; i++){
-        if(max_score<student[i].score){
+        if(max_score < student[i].score or i==0){
             max_score = student[i].score;
         }
     }
+    for(int i=0; i<size; i++){
+        if(student[i].score == max_score){
+            cout<<student[i].name<<" ";
+        }
+    }
+    cout<<": ";
     return max_score;
 }
 double MinStudent(Student student[],int size){
     int min_score = student[0].score;
+    string student_name;
     for(int i=0; i<size; i++){
-        if(min_score>student[i].score){
+        // cout<<student[i].name<<" ";
+        if(student[i].score < min_score or i == 0){
             min_score = student[i].score;
         }
+
     }
+    for(int i=0; i<size; i++){
+        if(student[i].score == min_score){
+            cout<<student[i].name<<" ";
+        }
+    }
+    
+    cout<<": ";
     return min_score;
 }
 double AvrScore(Student student[],int size){
@@ -34,7 +52,7 @@ double AvrScore(Student student[],int size){
 }
 
 double ModeScore(Student student[],int size){
-    double score_list[10];
+    double score_list[size];
     int size_list = sizeof(score_list)/sizeof(score_list[0]);
     for(int i=0; i<size;i++){
         score_list[i] = student[i].score;
@@ -58,23 +76,64 @@ double ModeScore(Student student[],int size){
     }
     return most_num;
 }
+double MedianScore(Student student[],int size){
+    double score_list[size];
+    int size_list = sizeof(score_list)/sizeof(score_list[0]);
+    for(int i=0; i<size; i++){
+        score_list[i] = student[i].score;
+    }
+    sort(score_list,score_list+size_list);
+    return (size_list%2==0)? (score_list[(size_list/2)-1]+score_list[size_list/2])/2 : score_list[size_list/2];
+    
+}
+double SDScore(Student student[],int size){
+    double x_bar = AvrScore(student,size), sum = 0;
+    for(int i=0; i<size; i++){
+        sum += pow((student[i].score - x_bar),2);
+    }
+    return sqrt(sum/(size-1));
+}
+void GradeStudent(Student student[], int size){
+    for(int i=0; i<size; i++){
+        if(student[i].score > AvrScore(student,size)+(SDScore(student,size)*2)){
+            student[i].grade ='A';
+        }
+        else if(student[i].score > AvrScore(student,size)+(SDScore(student,size))){
+            student[i].grade = 'B';
+        }
+        else if(student[i].score > AvrScore(student,size)){
+            student[i].grade = 'C';
+        }
+        else if(student[i].score > AvrScore(student,size)-(SDScore(student,size))){
+            student[i].grade = 'D';
+        }
+        else{
+            student[i].grade = 'F';
+        }
+        cout<<student[i].name<<" : "<<student[i].grade<<endl;
+    }
+}
 
 int main(){
     Student student[10];
     student[0] = {"John",60};
     student[1] = {"Ono",63};
-    student[2] = {"Non",77};
+    student[2] = {"Non",66};
     student[3] = {"Bryan",63};
     student[4] = {"Earth",77};
     student[5] = {"Mon",63};
-    student[6] = {"Toto",73};
-    student[7] = {"Boom",77};
-    student[8] = {"Dikolo",90};
+    student[6] = {"Toto",60};
+    student[7] = {"Boom",85};
+    student[8] = {"Dikolo",85};
     student[9] = {"Boss",77};
-    int size = (sizeof(Student)/(sizeof(student[0].score)));
+    int size = (sizeof(student)/(sizeof(student[0])));
 
+    // cout<<"size : "<<size<<endl;
     cout<<"Max score : "<<MaxStudent(student,size)<<endl;
     cout<<"Min score : "<<MinStudent(student,size)<<endl;
     cout<<"Average score : "<<AvrScore(student,size)<<endl;
     cout<<"Mode score : "<<ModeScore(student,size)<<endl;
+    cout<<"Median score : "<<MedianScore(student,size)<<endl;
+    cout<<"SD score : "<<SDScore(student,size)<<endl;
+    GradeStudent(student,size);
 }
