a58a7eac276d7349ada3a354ec8ad51bfb7a891f
Oceannnx <onopingping@gmail.com>
2023-02-09 14:44:58 +0700

    1.0

---
+++ python/date/date.py
@@ -0,0 +1,12 @@
+import datetime
+birth_day, birth_month, birth_year = (input("Enter : ")).split()
+pick_day, pick_month, pick_year = (input("Enter : ")).split()
+birthday = datetime.date(int(birth_year),int(birth_month),int(birth_day)) #datetime.date(YEAR,MONTH,DAY)
+pickday = datetime.date(int(birth_year),int(birth_month),int(birth_day)) #datetime.date(YEAR,MONTH,DAY)
+now = datetime.date.today()
+delta = now - birthday
+age = delta.days//365
+month = ((delta.days)-(age*365))//30
+day = (((delta.days) - (age*365))%30)
+print(age,month,day)
+print(f"Age : {age} Year {month} month {day} day")
